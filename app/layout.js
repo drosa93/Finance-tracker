ec51acc25814bf6fb9bc9085fcf2d63c7e58115c
drosa93 <DM_delarosa93@yahoo.com>
2025-02-11 13:31:34 -0600

    add income modal

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,19 @@
+"use client"
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Nav from "./components/navigation"; 
+import Nav from "./components/navigation";
+import FinanceContextProvider from "./lib/store/finance-context";
 
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <FinanceContextProvider>
         <Nav/>
         {children}
+        </FinanceContextProvider>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@
 import {useState} from "react";
 import { currencyFormatter } from "./lib/utils";
 import ExpenseCategoryItem from "./components/expenseItem";
+import AddIncomeModal from "./components/modals/addIncomeModal";
 import {Chart as ChartJs, ArcElement, Legend, Tooltip} from 'chart.js';
 import {Doughnut} from 'react-chartjs-2';
-import Modal from "./components/modal";
 
 ChartJs.register(ArcElement, Legend, Tooltip);
 
@@ -39,84 +39,76 @@ export default function Home() {
 
   const [showAddIncomeModal,setShowAddIncomeModal] = useState(false);
 
+ 
+
+
   return (
     <>
-    {/*Add Income Modal */}
-    <Modal show={showAddIncomeModal} onClose={setShowAddIncomeModal}>
-      <form className="flex flex-col gap-4 p-6">
-        <div className="input-group">
-          <label htmlFor="amount">Income Amount</label>
-          <input
-          type="number"
-          name="amount"
-          min={0.01}
-          step={0.01}
-          placeholder="Enter income amount"
-          required/>
-        </div>
+      {/* Add Income Modal */}
+     <AddIncomeModal
+     show={showAddIncomeModal}
+     onClose={setShowAddIncomeModal}
+     income={setShowAddIncomeModal}
+     />
 
-        <div className="input-group">
-          <label htmlFor="Description">Description</label>
-          <input 
-          type="text"
-          name="description"
-          placeholder="Enter income descritpion"
-          required/>
-        </div>
+      <main className="container max-w-2xl px-6 mx-auto">
+        <section className="py-3">
+          <small className="text-gray-400 text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+        </section>
 
-          <button type="submit" className="btn btn-primary">
-            Add Entry
+        <section className="flex items-center gap-2 py-3">
+          <button onClick={() => {}} className="btn btn-primary">
+            + Expenses
           </button>
-      </form>
-    </Modal>
+          <button
+            onClick={() => {
+              setShowAddIncomeModal(true);
+            }}
+            className="btn btn-primary-outline"
+          >
+            + Income
+          </button>
+        </section>
 
-    <main className="container max-w-2xl px-6 mx-auto">
-      <section className="py-3">
-        <small className="text-grey-400 text-md"> My Balance </small>
-        <h2 className="text-4xl font-bold">{currencyFormatter(1000)}</h2>
-      </section>
-   
-      <section className="flex items-center gap-2 py-3">
-        <button  onClick={() => {
-           setShowAddIncomeModal(true);
-          }}
-          className="btn btn-primary">+expenses</button>
-        <button className="btn btn-primary-outline">+income</button>
-      </section>
-
-      {/*expenses*/}
-      <section className="py-6">
-        <h3 className="text-2xl">My Expenses</h3>
-        <div className="flex flex-col gap-4 mt-6">
-          {DUMMY_DATA.map(expense => {
-            return ( <ExpenseCategoryItem
-              color={expense.color}
-              title={expense.title}
-              total={expense.total} />
-              )
+        {/* Expenses */}
+        <section className="py-6">
+          <h3 className="text-2xl">My Expenses</h3>
+          <div className="flex flex-col gap-4 mt-6">
+            {DUMMY_DATA.map((expense) => {
+              return (
+                <ExpenseCategoryItem
+                  key={expense.id}
+                  color={expense.color}
+                  title={expense.title}
+                  total={expense.total}
+                />
+              );
             })}
-        </div>
-      </section>
+          </div>
+        </section>
 
-      {/* Chart Section */}
-      <section className="py-6">
-        <h3 className="text-2xl">stats</h3>
-        <div className="w-1/2 mx-auto">
-          <Doughnut data={{
-            labels: DUMMY_DATA.map(expense => expense.title),
-            datasets: [
-              {
-                label: "Expenses",
-                data: DUMMY_DATA.map(expense => expense.total),
-                backgroundColor: DUMMY_DATA.map(expense => expense.color),
-                bolderColor: ['#fff'],
-                borderWidth: 1,
-              }
-            ]
-          }} />
-        </div>
-      </section>
-    </main>
+        {/* Chart Section */}
+        <section className="py-6">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto">
+            <Doughnut
+              data={{
+                labels: DUMMY_DATA.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: DUMMY_DATA.map((expense) => expense.total),
+                    backgroundColor: DUMMY_DATA.map((expense) => expense.color),
+                    borderColor: ["#18181b"],
+                    borderWidth: 5,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
     </>
   );
 }
