5f78651e16f0411b1ae91d6a14896f49204ab201
drosa93 <DM_delarosa93@yahoo.com>
2025-02-21 10:30:55 -0600

    fixed that last lil peice

--- app/lib/firebase/index.js
+++ app/lib/firebase/index.js
@@ -8,13 +8,13 @@ import { getAuth} from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyC7UwKsa35sKRbGkZ3IKyj89HsdXU8oIDc",
+  authDomain: "finance-tracker-d494f.firebaseapp.com",
+  projectId: "finance-tracker-d494f",
+  storageBucket: "finance-tracker-d494f.firebasestorage.app",
+  messagingSenderId: "653767700720",
+  appId: "1:653767700720:web:28be9953ced8570208d235",
+  measurementId: "G-T4NNN1BNNJ"
 };
 
 // Initialize Firebase
