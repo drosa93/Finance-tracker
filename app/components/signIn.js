0a95010676e68e0ff2e73f5707a8e65a2bea599c
drosa93 <DM_delarosa93@yahoo.com>
2025-02-21 11:04:15 -0600

    using email to sign up

--- app/components/signIn.js
+++ app/components/signIn.js
@@ -1,29 +1,97 @@
-import React, {useContext} from "react";
+"use Context"
+import { auth } from "../lib/firebase";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import {useContext, useEffect, useState} from "react";
 
-import { authContext } from "../lib/store/auth-context";
+//import { authContext } from "../lib/store/auth-context";
 
 import {FcGoogle} from "react-icons/fc";
 import { signInAnonymously } from "firebase/auth";
 
 function SignIn() {
-    const {googleLoginHandler} = useContext(authContext);
-    const {signIn, signUp} = useContext(authContext)
-    return (
-        <main className="container max-w-2xl px-6 mx-auto">
-            <h1 className="mb-6 text-6xl font-bold text-center">Welcome 👋👋🏽👋🏾👋🏿</h1>
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log(user.email);
+                //...
+            } else {
+                // Usser is signed out
+                // ...
+            }
+        });
+    }, []);
+
+    const signUp = async () => {
+        try {
+            const userCredential = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            console.log(userCredential.user.email);
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+    const login = async () => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
+            console.log(userCredential.user.email);
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+    const logout = async () => {
+        try { 
+            await signOut(auth);
+            console.log('signed out');
+        } catch (e) {
+            console.log(e)
+        }
+    }
+    // const {googleLoginHandler} = useContext(authContext);
+    // const {signIn, signUp} = useContext(authContext)
+     return (
+         <main className="container max-w-2xl px-6 mx-auto">
+             <h1 className="mb-6 text-6xl font-bold text-center">Welcome 👋👋🏽👋🏾👋🏿</h1>
 
             <div className="flex flex-col overflow-hidden shadow-md shadow-slate-500 bg-slate-800 rounded-2xl">
-                <div className="h-52">
-                </div>
+                 <div className="h-52">
+                    <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="put your email"
+                    />
+                    <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="password"
+                    />
+                    <button onClick={signUp}>SignUp</button>
+                    <button onCLick={SignIn}>Login</button>
+                    <button onClick={logout}>Sign Out</button>
 
-                <div className="px-4 py-4 ">
-                    <h3 className="text-2xl text-center">Please sign in to continue</h3>
+                 </div>
 
-                    <button onClick={googleLoginHandler} className="flex self-start gap-2 p-4 mx-auto mt-6 font-medium text-white align-middle bg-gray-700 rounded-lg">
-                        <FcGoogle className="text-2xl" />Google</button>
+                 <div className="px-4 py-4 ">
+                     <h3 className="text-2xl text-center">Please sign in to continue</h3>
+
+                     {/* <button onClick={googleLoginHandler} className="flex self-start gap-2 p-4 mx-auto mt-6 font-medium text-white align-middle bg-gray-700 rounded-lg">
+                         <FcGoogle className="text-2xl" />Google</button> */}
                 </div>
-            </div>
-        </main>
+             </div>
+         </main>
     );
 }
 
